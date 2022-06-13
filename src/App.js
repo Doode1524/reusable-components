ca6eb773035ddc6b6ef5824baef23cb66d762012
joey <doode1524@gmail.com>
2022-06-13 17:47:05 -0400

    add RRD and make 4 test components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Header from "./Header";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Products from "./Products";
 
 function App() {
+  const navigate = useNavigate()
+  
   return (
     <div>
       <Header />
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
