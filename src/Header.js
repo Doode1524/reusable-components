83a5575722fdf59cf6abe920fe3d7d7579a559e9
joey <doode1524@gmail.com>
2022-06-13 17:40:29 -0400

    add header component

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div>
+        <h1 className="header-title">My Header</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
